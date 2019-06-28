d4aba6352a94ad2fa4377f57d0be6c030c9ed246
Mardy A. dela Cruz <mardy.codev@gmail.com>
2019-06-28 16:34:58 +0800

    Updates

---
+++ precache-manifest.fbda955a153384078c0d4a28093c3311.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f77dcae5668246b1ad685a468590f26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40ccf1e204d25c83de78",
+    "url": "/static/css/main.148fb1d1.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "40ccf1e204d25c83de78",
+    "url": "/static/js/main.7965d643.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
